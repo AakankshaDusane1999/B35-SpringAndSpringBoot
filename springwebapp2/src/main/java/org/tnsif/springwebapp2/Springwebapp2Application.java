3e007237453acc9b82199c045aeb0e38c3da97a5
mrand <mrand@LAPTOP-FBEAVA6P>
2023-02-24 14:09:08 +0530

    1st commit

---
+++ springwebapp2/src/main/java/org/tnsif/springwebapp2/Springwebapp2Application.java
@@ -0,0 +1,17 @@
+package org.tnsif.springwebapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp2Application.class, args);
+		int x=15;
+		System.out.println(x);
+	}
+
+
+	}
+
